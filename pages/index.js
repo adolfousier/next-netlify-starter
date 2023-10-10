6ee5c48ddc4fa6dac7493196e6cd9847710e2594
Adolfo Usier <35583002+adolfousier@users.noreply.github.com>
2023-10-11 00:30:41 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to Mint Vetures demo app!" />
+        <Header title="Welcome to Mint Ventures demo app!" />
         <p className="description">
           Get started by clicking on the bot icon.
         </p>
