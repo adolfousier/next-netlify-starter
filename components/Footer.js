969b8334c0410493a82fcd4d4d4b476eaa4b3e2c
Adolfo Usier <35583002+adolfousier@users.noreply.github.com>
2023-10-11 09:54:33 +0100

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        <img src="/MV_favicon.svg" alt="Mint Ventures Logo" className={styles.logo} />
+        <img src="/Mint_Ventures_logo.png" alt="Mint Ventures Logo" className={styles.logo} />
     
       </footer>
     </>
