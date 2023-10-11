26f7c422c1e2335ecbbb6dbbfb0f5a5a4f4e562f
Adolfo Usier <35583002+adolfousier@users.noreply.github.com>
2023-10-11 10:01:52 +0100

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        <img src="/Mint_Ventures_logo.png" alt="Mint Ventures Logo" className={styles.logo} />
+        <img src="/mint_ventures_small.png" alt="Mint Ventures Logo" className={styles.logo} />
     
       </footer>
     </>
