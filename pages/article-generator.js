e3607f3f9ea968ec4c4c74429f06c08f798e7206
Adolfo Usier <35583002+adolfousier@users.noreply.github.com>
2023-10-12 17:24:58 +0100

    Create article-generator.js

---
+++ pages/article-generator.js
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Mint Ventures AI Demo!</title>
+        <link rel="icon" href="/MV_favicon.svg" />
+      </Head>
+
+      <main>
+        <Header title="Welcome to Mint Ventures AI Article Generator Demo!" />
+        <p className="description">
+          Too, two or to? Use our chatbot as a personal editor to double-check your grammar. Get started by clicking on the bot icon.
+        </p>
+      </main>
+
+      <Footer />
+    </div>
+  )
+}
